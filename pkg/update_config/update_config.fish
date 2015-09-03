c5c92ff5bf15a5a8a2418d5498e3a31057fb0ba6
Louis Borsu <louis@woorank.com>
2015-09-04 00:09:26 +0200

    Update with relative link

--- pkg/update_config/update_config.fish
+++ pkg/update_config/update_config.fish
@@ -2,10 +2,10 @@
 #     Update Script
 # ----------------------
 function update_config
-  sh $fish_custom/plugins/update_config/updates.sh
+  sh ~/.config/omf/pkg/update_config/lib/updates.sh
 end
 
 # Configure the paths below
-set -x CUSTOM_OMF_CONFIG_PATH $fish_custom
+set -x CUSTOM_OMF_CONFIG_PATH ~/.config/omf/
 set -x CUSTOM_ITERM_CONFIG_PATH ~/Github/iterm_config/
-set -x CUSTOM_SUBLIME_CONFIG_PATH ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User
+set -x CUSTOM_SUBLIME_CONFIG_PATH ~/.config/omf/pkg/update_config/ST3_pref
